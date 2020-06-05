03f8a25f169a91e248acc97fbeab6493bc7d0068
ecemarc <ece.marcelli@stern.nyu.edu>
2020-06-05 14:25:12 -0400

    Requested Info

---
+++ app/robo_advisor.py
@@ -0,0 +1,45 @@
+# app/robo_advisor.py
+
+import requests
+import json
+
+#
+# INFO INPUTS
+#
+#
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+response = requests.get(request_url)
+print(type(response))  # class
+print(response.status_code)  # we got 200
+print(response.text)  # string-need to import json module
+
+
+# parse use the json module called jason.loads to change response.text to dictionary
+parsed_response = json.loads(response.text)
+
+
+breakpoint()
+
+
+exit()
+#
+#
+#
+
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print("LATEST DAY: 2018-02-20")
+print("LATEST CLOSE: $100,000.00")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")

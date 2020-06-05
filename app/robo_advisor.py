c4e1c81110efd3b7ca33f71ccb33c83052a48538
ecemarc <ece.marcelli@stern.nyu.edu>
2020-06-05 14:43:29 -0400

    Latest_Refresh_Date

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -17,11 +17,11 @@ print(response.text)  # string-need to import json module
 # parse use the json module called jason.loads to change response.text to dictionary
 parsed_response = json.loads(response.text)
 
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-breakpoint()
+# breakpoint()
 
 
-exit()
 #
 #
 #
@@ -33,7 +33,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
